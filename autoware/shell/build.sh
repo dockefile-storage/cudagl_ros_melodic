93d3815fe076b7f0bb975cf08ca2a7b4cdf4978c
Jinagxumin <cjiangxumin@gmail.com>
2022-01-21 00:18:48 +0800

    updaet ....

---
+++ autoware/shell/build.sh
@@ -0,0 +1,12 @@
+##############################
+# build autoware
+##############################
+
+mkdir  -p Autoware/src 
+cd  Autoware
+
+vcs import src < /home/docker/autoware.ai.repos
+
+rosdep install -y --from-paths src --ignore-src --rosdistro $ROS_DISTRO
+
+AUTOWARE_COMPILE_WITH_CUDA=1 colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release
