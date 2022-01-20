d6e7bf2b5b538851e716f6a6c8981cc9ad275bb6
Jinagxumin <cjiangxumin@gmail.com>
2022-01-21 01:21:36 +0800

    update .....

--- autoware/shell/build.sh
+++ autoware/shell/build.sh
@@ -3,14 +3,17 @@
 ##############################
 
 su - docker -c "mkdir -p /home/docker/Autoware/src"
-su - docker -c "/bin/bash -c 'source /home/docker/.bash_aliases'"
-
-source /opt/ros/melodic/setup.bash && sudo rosdep init 
-
 su - docker -c "/bin/bash  -c 'source /home/docker/.bash_aliases' ;
-rosdep update ;
 cd  /home/docker/Autoware; 
 vcs import src < /home/docker/shell/autoware.ai.repos; 
+"
+
+source /opt/ros/melodic/setup.bash && sudo rosdep init ; 
+su - docker -c "/bin/bash  -c '/opt/ros/melodic/setup.bash; rosdep update' " ;
+
+cd  /home/docker/Autoware; 
 rosdep install -y --from-paths src --ignore-src --rosdistro $ROS_DISTRO ;  
+
+su - docker -c "cd  /home/docker/Autoware; 
 AUTOWARE_COMPILE_WITH_CUDA=1 colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release ;
 "
